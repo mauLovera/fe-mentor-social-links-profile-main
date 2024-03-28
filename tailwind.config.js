74afc72eb106931e9297b8ac86e06facd3067738
mauLovera <mauricioloveraedu@gmail.com>
2024-03-28 08:20:19 -0400

    feat: working build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./index.html"],
+	theme: {
+		colors: {
+			primary: "hsl(75, 94%, 57%)",
+			grey: "hsl(0, 0%, 20%)",
+			"dark-grey": "hsl(0, 0%, 12%)",
+			"off-black": "hsl(0, 0%, 8%)",
+			"white": "hsl(0, 0%, 100%)",
+		},
+	},
+	plugins: [],
 }
-
