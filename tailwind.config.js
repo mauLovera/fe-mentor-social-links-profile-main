789ba59ccb61bf765600002eb68e259dd4943ce1
mauLovera <mauricioloveraedu@gmail.com>
2024-04-30 18:26:43 -0400

    update: tw-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,18 @@ module.exports = {
 	content: ["./index.html"],
 	theme: {
 		colors: {
-			primary: "hsl(75, 94%, 57%)",
-			grey: "hsl(0, 0%, 20%)",
-			"dark-grey": "hsl(0, 0%, 12%)",
-			"off-black": "hsl(0, 0%, 8%)",
-			"white": "hsl(0, 0%, 100%)",
+			"primary-green": "hsl(75, 94%, 57%)",
+			"neutral-grey": "hsl(0, 0%, 20%)",
+			"neutral-dark-grey": "hsl(0, 0%, 12%)",
+			"neutral-off-black": "hsl(0, 0%, 8%)",
+			"neutral-white": "hsl(0, 0%, 100%)",
 		},
 		screens: {
 			sm: "400px"
 		},
+		maxWidth: {
+			card: "384px"
+		}
 	},
 	plugins: [],
 }
